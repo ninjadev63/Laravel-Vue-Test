5f1adc4fec48327daf516cd48600e996e0806106
Bohdan Lizavenko <webdev.rds@gmail.com>
2023-04-11 04:14:42 -0700

    set up infra with Laravel9, vuex, vue-router, vite

---
+++ resources/js/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+import toast from "./modules/toast";
+
+export default createStore({
+    state: {
+        //
+    },
+    mutations: {
+        //
+    },
+    actions: {
+        //
+    },
+    modules: {
+        toast,
+    },
+    plugins: [],
+});
