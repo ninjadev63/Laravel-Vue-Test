22433d671a845eafcbbbd8ac3ccc1caf268c840f
Bohdan Lizavenko <webdev.rds@gmail.com>
2023-04-12 00:28:08 -0700

    update user table migration

--- database/migrations/2023_04_12_071430_create_products_table.php
+++ database/migrations/2023_04_12_071430_create_products_table.php
@@ -6,31 +6,34 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('products', function (Blueprint $table) {
-					$table->id();
-					$table->string('title');
-					$table->integer('rating');
-					$table->float('price');
-					$table->string('image');
-					$table->string('categories_id');
-					$table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->id();
+      $table->string('title');
+      $table->integer('rating');
+      $table->float('price');
+      $table->string('image');
+      $table->foreignId('categories_id')
+        ->constrained('categories')
+        ->cascadeOnUpdate()
+        ->cascadeOnDelete();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('products');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('products');
+  }
 };
